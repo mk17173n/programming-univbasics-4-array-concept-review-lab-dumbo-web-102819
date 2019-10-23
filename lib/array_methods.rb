40daf5e68fcbe46f34f0f62966b2e07f6a11da59
Monsur Khan <monsur.khan@gmail.com>
2019-10-23 03:16:52 +0000

    Automatically backed up by Learn

--- lib/array_methods.rb
+++ lib/array_methods.rb
@@ -19,4 +19,14 @@ def find_maximum_value(array)
 end
 
 def find_min_value(array)
-
+def find_min_value(array)
+    minimum = array[0]  #Assume first element is the min
+   
+    array.length.times { |index|
+      if array[index] < minimum    
+          minimum = array[index]    
+      end
+    }
+   
+    return minimum
+end
